7f78b022219abaea112be946225dc5791c815b75
AiSY Yang <kai978854518@hotmail.com>
2017-12-13 18:00:31 +0800

    加入单元格对齐与选中同色单元格功能 限制聚光灯只在一个表内生效

--- Cells/Ribbon1.Designer.cs
+++ Cells/Ribbon1.Designer.cs
@@ -34,17 +34,30 @@ namespace Cells
 		/// </summary>
 		private void InitializeComponent()
 		{
+			Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl1 = this.Factory.CreateRibbonDropDownItem();
+			Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl2 = this.Factory.CreateRibbonDropDownItem();
+			Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl3 = this.Factory.CreateRibbonDropDownItem();
+			Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl4 = this.Factory.CreateRibbonDropDownItem();
 			this.tab1 = this.Factory.CreateRibbonTab();
 			this.group1 = this.Factory.CreateRibbonGroup();
 			this.Light = this.Factory.CreateRibbonCheckBox();
 			this.group2 = this.Factory.CreateRibbonGroup();
+			this.box1 = this.Factory.CreateRibbonBox();
 			this.editBox1 = this.Factory.CreateRibbonEditBox();
 			this.editBox2 = this.Factory.CreateRibbonEditBox();
+			this.splitChar = this.Factory.CreateRibbonComboBox();
+			this.separator1 = this.Factory.CreateRibbonSeparator();
+			this.concatenation = this.Factory.CreateRibbonButton();
+			this.group3 = this.Factory.CreateRibbonGroup();
+			this.ctrlL = this.Factory.CreateRibbonButton();
+			this.ctrlR = this.Factory.CreateRibbonButton();
 			this.button1 = this.Factory.CreateRibbonButton();
-			this.button2 = this.Factory.CreateRibbonButton();
+			this.colorDialog1 = new System.Windows.Forms.ColorDialog();
 			this.tab1.SuspendLayout();
 			this.group1.SuspendLayout();
 			this.group2.SuspendLayout();
+			this.box1.SuspendLayout();
+			this.group3.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tab1
@@ -52,13 +65,13 @@ namespace Cells
 			this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
 			this.tab1.Groups.Add(this.group1);
 			this.tab1.Groups.Add(this.group2);
+			this.tab1.Groups.Add(this.group3);
 			this.tab1.Label = "Cells";
 			this.tab1.Name = "tab1";
 			// 
 			// group1
 			// 
 			this.group1.Items.Add(this.Light);
-			this.group1.Items.Add(this.button2);
 			this.group1.Label = "group1";
 			this.group1.Name = "group1";
 			// 
@@ -70,34 +83,88 @@ namespace Cells
 			// 
 			// group2
 			// 
-			this.group2.Items.Add(this.editBox1);
-			this.group2.Items.Add(this.editBox2);
-			this.group2.Items.Add(this.button1);
-			this.group2.Label = "group2";
+			this.group2.Items.Add(this.box1);
+			this.group2.Items.Add(this.separator1);
+			this.group2.Items.Add(this.concatenation);
+			this.group2.Label = "并列同类项";
 			this.group2.Name = "group2";
+			// 
+			// box1
+			// 
+			this.box1.BoxStyle = Microsoft.Office.Tools.Ribbon.RibbonBoxStyle.Vertical;
+			this.box1.Items.Add(this.editBox1);
+			this.box1.Items.Add(this.editBox2);
+			this.box1.Items.Add(this.splitChar);
+			this.box1.Name = "box1";
 			// 
 			// editBox1
 			// 
 			this.editBox1.Label = "同类项列";
 			this.editBox1.Name = "editBox1";
-			this.editBox1.Text = null;
+			this.editBox1.Text = "E";
 			// 
 			// editBox2
 			// 
 			this.editBox2.Label = "并列项列";
 			this.editBox2.Name = "editBox2";
-			this.editBox2.Text = null;
+			this.editBox2.Text = "F";
+			// 
+			// splitChar
+			// 
+			ribbonDropDownItemImpl1.Label = "Tab(并列)";
+			ribbonDropDownItemImpl2.Label = "Space(空格)";
+			ribbonDropDownItemImpl3.Label = "-";
+			ribbonDropDownItemImpl4.Label = ",";
+			this.splitChar.Items.Add(ribbonDropDownItemImpl1);
+			this.splitChar.Items.Add(ribbonDropDownItemImpl2);
+			this.splitChar.Items.Add(ribbonDropDownItemImpl3);
+			this.splitChar.Items.Add(ribbonDropDownItemImpl4);
+			this.splitChar.Label = "分隔符 ";
+			this.splitChar.Name = "splitChar";
+			this.splitChar.Text = "Tab(并列)";
+			// 
+			// separator1
+			// 
+			this.separator1.Name = "separator1";
+			// 
+			// concatenation
+			// 
+			this.concatenation.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+			this.concatenation.Image = global::Cells.Properties.Resources.Exec;
+			this.concatenation.Label = "执行";
+			this.concatenation.Name = "concatenation";
+			this.concatenation.ShowImage = true;
+			this.concatenation.SuperTip = "主要用于一个条件匹配多个结果";
+			this.concatenation.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.Concatenation_Click);
+			// 
+			// group3
+			// 
+			this.group3.Items.Add(this.button1);
+			this.group3.Items.Add(this.ctrlL);
+			this.group3.Items.Add(this.ctrlR);
+			this.group3.Label = "单元格对齐";
+			this.group3.Name = "group3";
+			// 
+			// ctrlL
+			// 
+			this.ctrlL.Label = "单元格左对齐";
+			this.ctrlL.Name = "ctrlL";
+			this.ctrlL.SuperTip = "向左对齐所有单元格";
+			this.ctrlL.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.Align_Click);
+			// 
+			// ctrlR
+			// 
+			this.ctrlR.Label = "单元格右对齐";
+			this.ctrlR.Name = "ctrlR";
+			this.ctrlR.SuperTip = "向右对其所有单元格";
+			this.ctrlR.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.Align_Click);
 			// 
 			// button1
 			// 
-			this.button1.Label = "并列同类项";
+			this.button1.Label = "选中同色单元格";
 			this.button1.Name = "button1";
-			this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.Button1_Click);
-			// 
-			// button2
-			// 
-			this.button2.Label = "button2";
-			this.button2.Name = "button2";
+			this.button1.SuperTip = "选中当前工作表内同一种文字颜色及底色的所有单元格";
+			this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.SameFormat);
 			// 
 			// Ribbon1
 			// 
@@ -111,6 +178,10 @@ namespace Cells
 			this.group1.PerformLayout();
 			this.group2.ResumeLayout(false);
 			this.group2.PerformLayout();
+			this.box1.ResumeLayout(false);
+			this.box1.PerformLayout();
+			this.group3.ResumeLayout(false);
+			this.group3.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -120,10 +191,17 @@ namespace Cells
 		internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox Light;
 		internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
 		internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
-		internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
 		internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox1;
 		internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox2;
-		internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+		internal Microsoft.Office.Tools.Ribbon.RibbonBox box1;
+		internal Microsoft.Office.Tools.Ribbon.RibbonComboBox splitChar;
+		internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator1;
+		internal Microsoft.Office.Tools.Ribbon.RibbonButton concatenation;
+		internal Microsoft.Office.Tools.Ribbon.RibbonGroup group3;
+		internal Microsoft.Office.Tools.Ribbon.RibbonButton ctrlL;
+		internal Microsoft.Office.Tools.Ribbon.RibbonButton ctrlR;
+		private System.Windows.Forms.ColorDialog colorDialog1;
+		internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
 	}
 
 	partial class ThisRibbonCollection
--- Cells/Ribbon1.cs
+++ Cells/Ribbon1.cs
@@ -14,76 +14,185 @@ namespace Cells
 	public partial class Ribbon1
 	{
 		Excel.Application application;
+		public static Excel.Worksheet activeWorksheet;
+		Excel.Worksheet lastActiveWorksheet;
+
 		private void Cells_Load(object sender, RibbonUIEventArgs e)
 		{
 			application = Globals.ThisAddIn.Application;
+			Globals.ThisAddIn.Application.SheetSelectionChange += Application_SheetSelectionChange;
 		}
-
+		#region 聚光灯功能
+		Color lightColor = Color.CadetBlue;
 		private void Light_Click(object sender, RibbonControlEventArgs e)
 		{
 			if (Light.Checked)
 			{
 				if (MessageBox.Show("确认开启聚光灯功能么？这个功能将会影响撤销功能", "警告", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1) == DialogResult.OK)
 				{
-					Globals.ThisAddIn.Application.SheetSelectionChange += Application_SheetSelectionChange;
-					Application_SheetSelectionChange(new object(), application.ActiveCell);
+					activeWorksheet = application.ActiveSheet;
+					application.Selection.EntireRow.Interior.Color = lightColor;
+					application.Selection.EntireColumn.Interior.Color = lightColor;
 				}
 				else
 				{
 					Light.Checked = false;
-					Globals.ThisAddIn.Application.SheetSelectionChange -= Application_SheetSelectionChange;
 				}
 			}
 			else
 			{
-				Excel.Worksheet worksheet = Globals.ThisAddIn.Application.ActiveWorkbook.ActiveSheet;
-				worksheet.Cells.Interior.ColorIndex = -4142;
-				Globals.ThisAddIn.Application.SheetSelectionChange -= Application_SheetSelectionChange;
+				activeWorksheet.Cells.Interior.ColorIndex = -4142;
 			}
 		}
+
 
 		private void Application_SheetSelectionChange(object Sh, Excel.Range Target)
 		{
-			Excel.Worksheet worksheet = Globals.ThisAddIn.Application.ActiveWorkbook.ActiveSheet;
-			worksheet.Cells.Interior.ColorIndex = -4142;
-			Target.EntireRow.Interior.Color = Color.CadetBlue;
-			Target.EntireColumn.Interior.Color = Color.CadetBlue;
-			//application.OnUndo("聚光灯", "LightOnUnDo");
+			if (activeWorksheet == application.ActiveSheet)
+			{
+				if (Light.Checked)
+				{
+					activeWorksheet.Cells.Interior.ColorIndex = -4142;
+					Target.EntireRow.Interior.Color = lightColor;
+					Target.EntireColumn.Interior.Color = lightColor;
+				}
+			}
+			else
+			{
+				activeWorksheet.Cells.Interior.ColorIndex = -4142;
+				activeWorksheet = application.ActiveSheet;
+			}
+			//以下命令为撤销时显示的命令 暂时无法完成撤销功能
+			//application.OnUndo("聚光灯", "FunctionName");
 		}
 
-		private void Button1_Click(object sender, RibbonControlEventArgs e)
+		#endregion
+		#region 并列同类项功能
+		private void Concatenation_Click(object sender, RibbonControlEventArgs e)
 		{
-			int row = 2;
-			int columns = 1;
 			Excel.Worksheet worksheet = Globals.ThisAddIn.Application.ActiveWorkbook.ActiveSheet;
 			Range eqrange = worksheet.Columns[editBox1.Text.ToUpper()];
 			Range neqrange = worksheet.Columns[editBox2.Text.ToUpper()];
-			Range eq0, eq1, neq0, neq1;
-			eq0 = eqrange.Rows[1];
-			neq0 = neqrange.Rows[1];
-			eq1 = eqrange.Rows[2];
-			neq1 = neqrange.Rows[2];
-			while (eq1.Value != null)
+			int baseRow = 1;
+			int row = 2;
+			int compareColumn = eqrange.Column;
+			int column = neqrange.Column;
+			int deltaColumn = 1;
+			Range baseCell = worksheet.Cells[baseRow, compareColumn];
+			Range cell = worksheet.Cells[row, compareColumn];
+			while (cell.Value != null)
 			{
-				if (eq1.Value == eq0.Value)
+				try
 				{
-					worksheet.Cells[neq0.Row, neq1.Column + columns].Value = neq1.Value;
-					columns++;
-					worksheet.Rows[row].Delete(XlDeleteShiftDirection.xlShiftUp);
-					eq1 = eqrange.Rows[row];
-					neq1 = neqrange.Rows[row];
-					continue;
+					if (cell.Value == baseCell.Value)
+					{
+						switch (splitChar.Text)
+						{
+							case "Tab(并列)":
+								worksheet.Cells[baseRow, column + deltaColumn].Value = worksheet.Cells[row, column].Value;
+								deltaColumn++;
+								break;
+							case "Space(空格)":
+								worksheet.Cells[baseRow, column].Value = worksheet.Cells[baseRow, column].Value + ' ' + (worksheet.Cells[row, column].Value).ToString();
+								break;
+							default:
+								worksheet.Cells[baseRow, column].Value = worksheet.Cells[baseRow, column].Value + splitChar.Text + (worksheet.Cells[row, column].Value).ToString();
+								break;
+						}
+						cell.EntireRow.Delete();
+						cell = worksheet.Cells[row, compareColumn];
+						continue;
+					}
+					else
+					{
+						baseRow++;
+						row++;
+						deltaColumn = 1;
+						baseCell = worksheet.Cells[baseRow, compareColumn];
+						cell = worksheet.Cells[row, compareColumn];
+					}
 				}
-				else
+				catch (Exception)
 				{
-					columns = 1;
-					eq0 = eq1.Cells;
-					neq0 = neq1.Cells;
+					baseCell.Interior.Color = Color.Red;
+					baseRow++;
+					row++;
+					baseCell = worksheet.Cells[baseRow, compareColumn];
+					cell = worksheet.Cells[row, compareColumn];
 				}
-				row++;
-				eq1 = eqrange.Rows[row];
-				neq1 = neqrange.Rows[row];
 			}
+
+		}
+		#endregion
+
+		private void Align_Click(object sender, RibbonControlEventArgs e)
+		{
+			activeWorksheet = application.ActiveSheet;
+			switch ((sender as RibbonButton).Label)
+			{
+				case "左对齐":
+					{
+						Range sel = application.Selection;
+						for (int i = sel.Row; i < sel.Row + sel.Rows.Count; i++)
+						{
+							Range cell = activeWorksheet.Cells[i, sel.Column];
+							for (int j = sel.Column; j < sel.Column + sel.Columns.Count; j++)
+							{
+
+								dynamic value = activeWorksheet.Cells[i, j].Value;
+								if (value != null)
+								{
+									activeWorksheet.Cells[i, j].Value = null;
+									cell.Value = value;
+									cell = cell.Next;
+								}
+							}
+						}
+						break;
+					}
+				case "右对齐":
+					{
+						Range sel = application.Selection;
+						for (int i = sel.Row; i < sel.Row + sel.Rows.Count; i++)
+						{
+							Range cell = activeWorksheet.Cells[i, sel.Column + sel.Columns.Count - 1];
+							for (int j = sel.Column + sel.Columns.Count - 1; j >= sel.Column; j--)
+							{
+								dynamic value = activeWorksheet.Cells[i, j].Value;
+								if (value != null)
+								{
+									activeWorksheet.Cells[i, j].Value = null;
+									cell.Value = value;
+									cell = cell.Offset[0, -1];
+
+								}
+							}
+						}
+						break;
+					}
+				default:
+					break;
+			}
+		}
+
+		private void SameFormat(object sender, RibbonControlEventArgs e)
+		{
+			//application.ScreenUpdating = false;
+			activeWorksheet = application.ActiveSheet;
+			Range cell = application.Selection;
+			Range selectRange = cell.Cells[1, 1];
+			foreach (Range item in activeWorksheet.UsedRange)
+			{
+				if (item.DisplayFormat.Font.Color == cell.DisplayFormat.Font.Color)
+				{
+					if (item.DisplayFormat.Interior.Color == cell.DisplayFormat.Interior.Color)
+					{
+						selectRange = application.Union(selectRange, item);
+					}
+				}
+			}
+			selectRange.Select();
+			//application.ScreenUpdating = true;
 		}
 	}
 }
